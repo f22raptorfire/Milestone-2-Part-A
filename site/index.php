efe5d34fa1a77dba07981104d7eefb57a99561e1
f22raptorfire <f22raptorfire@yahoo.com>
2013-11-01 15:38:47 -0700

    Add default image. Add DOCTYPE. Fix HTML formatting.

--- site/index.php
+++ site/index.php
@@ -1,10 +1,10 @@
-
+<!DOCTYPE html>
 <html>
-<head>
-<title>Home</title>
-<link rel="stylesheet" type="text/css" href="casablanca.css">
-</head>
-<body>
+ <head>
+   <title>Home</title>
+   <link rel="stylesheet" type="text/css" href="casablanca.css">
+ </head>
+ <body>
 <?php
 $connection = mysql_connect('localhost', 'root', ''); //The Blank string is the password
 mysql_select_db('courses');
@@ -13,15 +13,22 @@ $query = "SELECT * FROM canvas"; //You don't need a ; like you do in SQL
 $result = mysql_query($query);
 $col_name = array();
 // start a table tag in the HTML
-echo "<table>\n\t<tr>\n\t\t<thead>\n";
+//prison of mirrors
+echo <<< END
+   <table>
+     <tr>
+       <thead>\n
+END;
 for ($i=0; $i < mysql_num_fields($result); $i++) {
 	$col_name[$i] = mysql_field_name($result, $i);
-	echo "\t\t\t<th scope=\"col\">" . $col_name[$i] . "</th>\n";
+	echo "         <th scope=\"col\">" . $col_name[$i] . "</th>\n";
 }
-echo "\t\t</thead>\n\t</tr>\n";
-
+echo <<<END
+       </thead>
+      </tr>\n
+END;
 while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
-	echo "\t<tr>\n";
+	echo "      <tr>\n";
 	for ($i=0; $i < mysql_num_fields($result); $i++) {
 		$temp = $row[$col_name[$i]];
 		if(!empty($temp)) {
@@ -30,14 +37,15 @@ while($row = mysql_fetch_array($result)){   //Creates a loop to loop through res
 			} else {
 				$temp = "<p>{$temp}</p>";
 			}
+		} else {
+			$temp = '<img src="http://i51.tinypic.com/91lc2f.gif" border="0" alt="Image and video hosting by TinyPic">';
 		}
-		echo "\t\t<td>" . $temp . "</td>\n";
+		echo "        <td>" . $temp . "</td>\n";
 	}
-	echo "\t</tr>\n";
+	echo "       </tr>\n";
 }
-echo "</table>\n"; //Close the table in HTML
-
+echo "   </table>\n"; //Close the table in HTML
 mysql_close(); //Make sure to close out the database connection
 ?>
-</body>
+ </body>
 </html>
