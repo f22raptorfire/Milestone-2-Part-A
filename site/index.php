30f4ef2620c708e41c39657a566db48003305435
f22raptorfire <f22raptorfire@yahoo.com>
2013-11-01 23:10:15 -0700

    format php code

--- site/index.php
+++ site/index.php
@@ -6,46 +6,72 @@
  </head>
  <body>
 <?php
-$connection = mysql_connect('localhost', 'root', ''); //The Blank string is the password
-mysql_select_db('courses');
-
-$query = "SELECT * FROM canvas"; //You don't need a ; like you do in SQL
-$result = mysql_query($query);
-$col_name = array();
+$connection = mysql_connect( 'localhost', 'root', '' ); //The Blank string is the password.
+if (!$connection)
+{
+ die('Could not connect: ' . mysql_error());
+}
+mysql_select_db( 'courses' );
+$query    = "SELECT * FROM canvas"; //You don't need a ; like you do in SQL
+$result   = mysql_query( $query );
+$col_name = array( );
 // start a table tag in the HTML
-echo <<< END
-   <table>
-     <tr>
-       <thead>\n
-END;
-for ($i=0; $i < mysql_num_fields($result); $i++) {
-	$col_name[$i] = mysql_field_name($result, $i);
-	echo "         <th scope=\"col\">" . $col_name[$i] . "</th>\n";
+?>
+  <table>
+     <thead>
+       <tr>
+<?php
+for ( $i = 0; $i < mysql_num_fields( $result ); $i++ )
+{
+ $col_name[ $i ] = mysql_field_name( $result, $i );
+ echo "         <th scope=\"col\">" . $col_name[ $i ] . "</th>\n";
 }
-echo <<<END
-       </thead>
-      </tr>\n
-END;
-while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
-	echo "      <tr>\n";
-	for ($i=0; $i < mysql_num_fields($result); $i++) {
-		$temp = $row[$col_name[$i]];
-		if(!empty($temp)) {
-			if(preg_match('/(download\?|(jpg|png|gif|bmp)$)/i', $temp)) {
-				$temp = "<img width =\"130px\" heigth=\"95px\" src=\"{$temp}\">";
-			} else if(preg_match('/^http/', $temp)) {
-				$temp = "<a href=\"{$temp}\">Link</a>";
-			} else {
-				$temp = "<p>{$temp}</p>";
-			}
-		} else {
-			$temp = '<img width ="130px" heigth="95px" src="https://scontent-a-sjc.xx.fbcdn.net/hphotos-prn1/250250_257757674235389_5197433_n.jpg">';
-		}
-		echo "        <td>" . $temp . "</td>\n";
-	}
-	echo "       </tr>\n";
+?>
+       </tr>
+      </thead>
+      <tfoot> <tr>
+<?php
+echo str_repeat( "<td></td>", mysql_num_fields( $result ) );
+?>
+       </tr> 
+      </tfoot>
+      <tbody>
+<?php
+while ( $row = mysql_fetch_array( $result ) )
+//Creates a loop to loop through results
+{
+ echo "      <tr>\n";
+ for ( $i = 0; $i < mysql_num_fields( $result ); $i++ )
+ {
+  $temp = $row[ $col_name[ $i ] ];
+  if ( !empty( $temp ) )
+  {
+   if ( preg_match( '/(download\?|(jpg|png|gif|bmp)$)/i', $temp ) )
+   {
+    $temp = "<img height=\"95px\" src=\"{$temp}\">";
+   }
+   else if ( preg_match( '/^http/', $temp ) )
+   {
+    $temp = "<a href=\"{$temp}\">Link</a>";
+   }
+   else
+   {
+    $temp = "<p>{$temp}</p>";
+   }
+  }
+  else
+  {
+   $temp = '<img width="95px" height="95px" src="http://www.travelervip.com/skin/frontend/default/travelervip/images/facebook-default-no-profile-pic.jpg">';
+  }
+  echo "        <td>" . $temp . "</td>\n";
+ }
+ echo "       </tr>\n";
 }
-echo "   </table>\n"; //Close the table in HTML
+?>
+  </tbody>
+   </table>
+<?php
+// Close the table in HTML
 mysql_close(); //Make sure to close out the database connection
 ?>
  </body>
